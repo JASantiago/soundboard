8a831d17962cb45aa617cf320cd3bff7bede5999
Jose  Santiago <josesantiluna@gmail.com>
2018-11-28 11:08:05 -0400

    playback-logic: - Audio can be played and stopped - Audio will playback while the app is in the background - Collection ready for adding more sounds/cells programmatically

---
+++ sound board/BoardCell.swift
@@ -0,0 +1,17 @@
+//
+//  BoardCell.swift
+//  sound board
+//
+//  Created by Jose  Santiago on 11/28/18.
+//  Copyright © 2018 Jose  Santiago. All rights reserved.
+//
+
+import UIKit
+
+class BoardCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var buttonAppearance: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
